5998cf60c5769f0afdb8338e5de502884f0e73a5
liviu1406 <41875519+liviu1406@users.noreply.github.com>
2023-09-21 00:11:33 +0100

    updated algos

--- src/AStarSearch.java
+++ src/AStarSearch.java
@@ -1,56 +1,125 @@
 import java.util.*;
 
+class Node {
+    public String name;
+    public int g;
+    public int h;
+    public Node parent;
+
+    public Node(String name, int h) {
+        this.name = name;
+        this.h = h;
+        this.g = Integer.MAX_VALUE;
+        this.parent = null;
+    }
+
+    public int f() {
+        return g + h;
+    }
+}
+
 public class AStarSearch {
+    private static Map<String, Node> nodes = new HashMap<>();
+    private static Map<String, List<Node>> adjList = new HashMap<>();
+    private static Map<String, Map<String, Integer>> stepCosts = new HashMap<>();
 
     public static void main(String[] args) {
-        State A = StateConnectionsUtil.A; // Access starting state from StateConnectionsUtil
+        initializeGraph();
+        Node start = nodes.get("A");
+        Node goal = nodes.get("H");
+        List<Node> path = aStarSearch(start, goal);
+        printPath(path);
+    }
 
-        Map<State, Integer> gValues = SearchUtils.initializeGValues(A);
-        PriorityQueue<State> frontier = SearchUtils.initializeFrontier(A, gValues);
+    private static void initializeGraph() {
+        nodes.put("A", new Node("A", 5));
+        nodes.put("B", new Node("B", 6));
+        nodes.put("C", new Node("C", 8));
+        nodes.put("D", new Node("D", 4));
+        nodes.put("E", new Node("E", 4));
+        nodes.put("F", new Node("F", 5));
+        nodes.put("G", new Node("G", 2));
+        nodes.put("H", new Node("H", 0));
 
+        addEdge("A", "B", 3);
+        addEdge("A", "C", 3);
+        addEdge("B", "D", 2);
+        addEdge("C", "F", 3);
+        addEdge("D", "E", 4);
+        addEdge("E", "F", 1);
+        addEdge("E", "G", 2);
+        addEdge("F", "G", 3);
+        addEdge("G", "H", 2);
+    }
 
-        Set<State> explored = new HashSet<>();
-        List<String> progression = new ArrayList<>();
+    private static void addEdge(String from, String to, int cost) {
+        adjList.computeIfAbsent(from, k -> new ArrayList<>()).add(nodes.get(to));
+        adjList.computeIfAbsent(to, k -> new ArrayList<>()).add(nodes.get(from));
 
-        System.out.println("RESULT");
+        stepCosts.computeIfAbsent(from, k -> new HashMap<>()).put(to, cost);
+        stepCosts.computeIfAbsent(to, k -> new HashMap<>()).put(from, cost);
+    }
 
-        while (!frontier.isEmpty()) {
-            State current = frontier.poll();
-            explored.add(current);
-            progression.add(current.name);
+    private static List<Node> aStarSearch(Node start, Node goal) {
+        PriorityQueue<Node> openList = new PriorityQueue<>(Comparator.comparingInt(Node::f));
+        Set<Node> closedList = new HashSet<>();
 
-            System.out.println("Expand: " + current.name + ", f=" + (gValues.get(current) + current.heuristic));
-            System.out.println("Frontier: " + frontierToString(frontier, gValues));
+        start.g = 0;
+        openList.add(start);
 
-            if (current == StateConnectionsUtil.H) {
-                System.out.println("Reached goal state: " + current.name);
-                System.out.println("The path taken is: " + progression);
-                System.out.println("The cost of this path is: " + gValues.get(current));
-                return;
+        while (!openList.isEmpty()) {
+            Node current = openList.poll();
+
+            if (current.equals(goal)) {
+                return reconstructPath(current);
             }
 
-            for (Connection conn : current.connections) {
-                State neighbor = conn.state;
-                int tentativeGValue = gValues.get(current) + conn.cost;
-                if (!explored.contains(neighbor) && !frontier.contains(neighbor)) {
-                    gValues.put(neighbor, tentativeGValue);
-                    frontier.add(neighbor);
-                } else if (tentativeGValue < gValues.getOrDefault(neighbor, Integer.MAX_VALUE)) {
-                    frontier.remove(neighbor);
-                    gValues.put(neighbor, tentativeGValue);
-                    frontier.add(neighbor);
+            closedList.add(current);
+
+            for (Node neighbor : adjList.get(current.name)) {
+                if (closedList.contains(neighbor)) {
+                    continue;
+                }
+
+                int tentativeG = current.g + stepCosts.get(current.name).get(neighbor.name);
+                if (tentativeG < neighbor.g) {
+                    neighbor.parent = current;
+                    neighbor.g = tentativeG;
+
+                    if (openList.contains(neighbor)) {
+                        openList.remove(neighbor);
+                    }
+
+                    openList.add(neighbor);
                 }
             }
         }
 
-        System.out.println("Goal state not reached");
+        return null; // No path found
     }
 
-    private static String frontierToString(PriorityQueue<State> frontier, Map<State, Integer> gValues) {
-        List<String> states = new ArrayList<>();
-        for (State state : frontier) {
-            states.add(state.name + "(f=" + (gValues.get(state) + state.heuristic) + ")");
+    private static List<Node> reconstructPath(Node node) {
+        List<Node> path = new ArrayList<>();
+        while (node != null) {
+            path.add(node);
+            node = node.parent;
         }
-        return states.toString();
+        Collections.reverse(path);
+        return path;
+    }
+
+    private static void printPath(List<Node> path) {
+        if (path == null) {
+            System.out.println("No path found.");
+            return;
+        }
+
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i).name);
+            if (i < path.size() - 1) {
+                System.out.print(" -> ");
+            }
+        }
+        System.out.println("\nTotal Cost: " + path.get(path.size() - 1).g);
     }
 }
--- src/AStarTreeSearch.java
+++ src/AStarTreeSearch.java
@@ -1,51 +1,112 @@
 import java.util.*;
 
-public class AStarTreeSearch {
+class NodeTree {
+    public String name;
+    public int g;
+    public int h;
+    public NodeTree parent;
 
-    public static void main(String[] args) {
-        State A = StateConnectionsUtil.A; // Access starting state from StateConnectionsUtil
-
-        Map<State, Integer> gValues = SearchUtils.initializeGValues(A);
-        PriorityQueue<State> frontier = SearchUtils.initializeFrontier(A, gValues);
-
-        List<String> progression = new ArrayList<>();
-
-        System.out.println("RESULT");
-
-        while (!frontier.isEmpty()) {
-            State current = frontier.poll();
-            progression.add(current.name);
-
-            System.out.println("Expand: " + current.name + ", f=" + (gValues.get(current) + current.heuristic));
-            System.out.println("Frontier: " + frontierToString(frontier, gValues));
-
-            if (current == StateConnectionsUtil.H) {
-                System.out.println("Reached goal state: " + current.name);
-                System.out.println("The path taken is: " + progression);
-                System.out.println("The cost of this path is: " + gValues.get(current));
-                return;
-            }
-
-            for (Connection conn : current.connections) {
-                State neighbor = conn.state;
-                int tentativeGValue = gValues.get(current) + conn.cost;
-
-                if (tentativeGValue < gValues.getOrDefault(neighbor, Integer.MAX_VALUE)) {
-                    gValues.put(neighbor, tentativeGValue);
-                    frontier.add(neighbor);
-                }
-            }
-        }
-
-        System.out.println("Goal state not reached");
+    public NodeTree(String name, int h) {
+        this.name = name;
+        this.h = h;
+        this.g = Integer.MAX_VALUE;
+        this.parent = null;
     }
 
-    private static String frontierToString(PriorityQueue<State> frontier, Map<State, Integer> gValues) {
-        List<String> states = new ArrayList<>();
-        for (State state : frontier) {
-            states.add(state.name + "(f=" + (gValues.get(state) + state.heuristic) + ")");
-        }
-        return states.toString();
+    public int f() {
+        return g + h;
     }
 }
 
+public class AStarTreeSearch {
+    private static Map<String, NodeTree> nodes = new HashMap<>();
+    private static Map<String, List<NodeTree>> adjList = new HashMap<>();
+    private static Map<String, Map<String, Integer>> stepCosts = new HashMap<>();
+
+    public static void main(String[] args) {
+        initializeGraph();
+        NodeTree start = nodes.get("A");
+        NodeTree goal = nodes.get("H");
+        List<NodeTree> path = aStarTreeSearch(start, goal);
+        printPath(path);
+    }
+
+    private static void initializeGraph() {
+        nodes.put("A", new NodeTree("A", 5));
+        nodes.put("B", new NodeTree("B", 6));
+        nodes.put("C", new NodeTree("C", 8));
+        nodes.put("D", new NodeTree("D", 4));
+        nodes.put("E", new NodeTree("E", 4));
+        nodes.put("F", new NodeTree("F", 5));
+        nodes.put("G", new NodeTree("G", 2));
+        nodes.put("H", new NodeTree("H", 0));
+
+        addEdge("A", "B", 3);
+        addEdge("A", "C", 3);
+        addEdge("B", "D", 2);
+        addEdge("C", "F", 3);
+        addEdge("D", "E", 4);
+        addEdge("E", "F", 1);
+        addEdge("E", "G", 2);
+        addEdge("F", "G", 3);
+        addEdge("G", "H", 2);
+    }
+
+    private static void addEdge(String from, String to, int cost) {
+        adjList.computeIfAbsent(from, k -> new ArrayList<>()).add(nodes.get(to));
+        adjList.computeIfAbsent(to, k -> new ArrayList<>()).add(nodes.get(from));
+
+        stepCosts.computeIfAbsent(from, k -> new HashMap<>()).put(to, cost);
+        stepCosts.computeIfAbsent(to, k -> new HashMap<>()).put(from, cost);
+    }
+
+    private static List<NodeTree> aStarTreeSearch(NodeTree start, NodeTree goal) {
+        PriorityQueue<NodeTree> openList = new PriorityQueue<>(Comparator.comparingInt(NodeTree::f));
+
+        start.g = 0;
+        openList.add(start);
+
+        while (!openList.isEmpty()) {
+            NodeTree current = openList.poll();
+
+            if (current.name.equals(goal.name)) {
+                return reconstructPath(current);
+            }
+
+            for (NodeTree neighbor : adjList.get(current.name)) {
+                NodeTree child = new NodeTree(neighbor.name, neighbor.h);
+                child.g = current.g + stepCosts.get(current.name).get(neighbor.name);
+                child.parent = current;
+
+                openList.add(child);
+            }
+        }
+
+        return null; // No path found
+    }
+
+    private static List<NodeTree> reconstructPath(NodeTree node) {
+        List<NodeTree> path = new ArrayList<>();
+        while (node != null) {
+            path.add(node);
+            node = node.parent;
+        }
+        Collections.reverse(path);
+        return path;
+    }
+
+    private static void printPath(List<NodeTree> path) {
+        if (path == null) {
+            System.out.println("No path found.");
+            return;
+        }
+
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i).name);
+            if (i < path.size() - 1) {
+                System.out.print(" -> ");
+            }
+        }
+        System.out.println("\nTotal Cost: " + path.get(path.size() - 1).g);
+    }
+}
